6e4ca07491ccf3eb2df68b3c4a1e6040e1a58a81
Yuta Matsumura <tkmbmbr@gmail.com>
2022-11-17 22:36:22 +0900

    Add unit testing

---
+++ tests/Services/UserServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using api.Models;
+using api.Services;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace api.test.Services;
+
+public class UserServiceTest
+{
+    [Fact]
+    void ListUsersAsyncTest()
+    {
+        MinimalDbContext dbContext = GetDbContext();
+        UserService service = new(dbContext);
+
+        List<User> actual = service.ListUsersAsync().Result;
+        Assert.Equal(5, actual.Count);
+    }
+
+    MinimalDbContext GetDbContext()
+    {
+        DbContextOptionsBuilder<MinimalDbContext> builder = new();
+        builder.UseInMemoryDatabase("minimalapisample");
+
+        MinimalDbContext dbContext = new(builder.Options);
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+
+        return dbContext;
+    }
+}
